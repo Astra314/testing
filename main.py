4d6a6e956565609fdf797fbd8b98bb57d7b8a87a
Julia <109692652+Astra314@users.noreply.github.com>
2023-02-17 10:14:28 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# Check for balanced parentheses in an expression
+open_list = ["[","{","("]
+close_list = ["]","}",")"]
+ 
+# Function to check parentheses
+def check(myStr):
+    stack = []
+    for i in range(len(myStr)):
+        if myStr[i] in open_list:
+            stack.append((myStr[i], i))
+        elif myStr[i] in close_list:
+            pos = close_list.index(myStr[i])
+            if ((len(stack) > 0) and
+                (open_list[pos] == stack[-1][0])):
+                stack.pop()
+            else:
+                return i+1
+    
+    # print("The stack is: ", stack)
+    if len(stack) == 0:
+        return "Success"
+    else:
+        return stack[-1][1]+2
+    
+ 
+ 
+# Driver code
+string = "{{{}}}"
+print(f"{string}: {check(string)}")
+ 
+string = "h[{}{})(]"
+print(f"{string}: {check(string)}")
+ 
+string = "{[}"
+print(f"{string}: {check(string)}")
